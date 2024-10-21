3335e1d94550efdbdbda7f994d7a0632e17124d9
VASILEIOS GKOUDINAKIS <agv132703@gmail.com>
2024-10-21 22:23:23 +0300

    Lab2 Complete

--- lab02.s
+++ lab02.s
@@ -12,6 +12,17 @@ prog:
 #-----------------------------
 # Write your code here!
 # Do not remove the prog label or write code above it!
+     add s0,zero,zero
+ repeat:
+     beq a1,zero,done
+     lw t1,0(a0)
+     bne t1,a2,skip
+     add s0,a0,zero
+skip:
+    addi a0,a0,4
+    addi a1,a1,-1
+    j repeat
+    
 #-----------------------------
 done:
     addi a7, zero, 10 
